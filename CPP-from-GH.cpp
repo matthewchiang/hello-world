ef230958b1b633b642b4f0965d9ab515b420c4a0
matthewchiang <matthewchiang18@gmail.com>
2016-01-10 18:09:17 -0500

    Create CPP-from-GH.cpp

---
+++ CPP-from-GH.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+cout << "Hello world!" << endl;
+cout << "This code is written directly in GitHub's text editor" << endl;
+cout << "Please run THIS FILE directly in VS" << endl;
+
+
+
+
+return 0;
+}
